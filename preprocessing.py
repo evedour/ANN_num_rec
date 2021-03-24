611a6cc3bd423bda4c488c42879eb02c50c63896
Eva Ntourou <64539498+evedour@users.noreply.github.com>
2021-03-24 21:48:49 +0200

    label fix

--- preprocessing.py
+++ preprocessing.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 df = pd.read_csv('datasets/mnist_train.csv')
 df_norm = (df.iloc[2:, :] - df.iloc[2:, :].mean())/df.iloc[2:, :].std()
-df_norm.to_csv('train_standarized.csv', index=False, header=False)
+rslt =  pd.concat([df_norm, df.iloc[:, 1]], axis=1)
+rslt.to_csv('train_normalized.csv', index=False, header=False)
--- preprocessing_0to1.py
+++ preprocessing_0to1.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 df = pd.read_csv('datasets/mnist_train.csv')
 df_norm = (df.iloc[2:, :])/255
-df_norm.to_csv('train_normalized.csv', index=False, header=False)
+rslt =  pd.concat([df_norm, df.iloc[:, 1]], axis=1)
+rslt.to_csv('train_normalized.csv', index=False, header=False)
