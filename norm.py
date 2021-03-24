f902b83fb7638c78a5d6ffac257f6d55f7818708
Eva Ntourou <64539498+evedour@users.noreply.github.com>
2021-03-24 21:07:19 +0200

    Delete norm.py
    needed to re-upload under different name

--- norm.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('datasets/mnist_train.csv')
-df_norm = (df.iloc[2:, :] )/255
-rslt = pd.concat([df_norm, df.iloc[1, :]]) #adds label row
-rslt.to_csv('example.csv', index=False, header=False)
