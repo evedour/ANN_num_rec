d6f770d8a1950390505c55433396c654ddda9f78
Eva Ntourou <64539498+evedour@users.noreply.github.com>
2021-04-10 20:27:25 +0300

    add main.py
    replaced MLP.py with main.py, functioning 5-fold CV single layer MLP

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
-# This is a sample Python script.
+import tensorflow
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import KFold
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+features = 784
+classes = 10
 
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(x_train.shape[0], features)
+x_test = x_test.reshape(x_test.shape[0], features)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#this is a test change
+#normalize
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+#labels
+y_train = to_categorical(y_train, classes)
+y_test = to_categorical(y_test, classes)
 
+input_shape = (features,)
+print(f'Feature shape: {input_shape}')
+# Create the model
+model = Sequential()
+model.add(Dense(350, input_shape=input_shape, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(classes, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', 'mse'])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+fold = 1
+kfold = KFold(5, shuffle=True, random_state=1)
+for train, test in kfold.split(x_train):
+    xi_train, xi_test = x_train[train], x_train[test]
+    yi_train, yi_test = y_train[train], y_train[test]
+    print(f' fold # {fold}, TRAIN: {train}, TEST: {test}')
+    fold = fold + 1
+    model.fit(xi_train, yi_train, epochs=10, batch_size=250, verbose=1, validation_split=0.2)
+    #Test the model after training
+    test_results = model.evaluate(xi_test, yi_test, verbose=1)
+    print(f'Test results in fold # {fold} - Loss: {test_results[0]} - Accuracy: {test_results[1]}% - MSE {test_results[2]}')
