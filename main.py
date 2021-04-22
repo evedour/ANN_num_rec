ff26fbc50401e9ebe0072a4cf6ba0266f70f3815
Eva Ntourou <evedour@gmail.com>
2021-04-22 17:43:52 +0300

    added 'run all option'

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ flag = True
 print('Project Υπολογιστικής Νοημοσύνης - Μέρος Α')
 while flag:
     rn = input('Επιλέξτε ερώτημα: \n2\n3\n4\n5\n')
+    if rn == 'all':
+        single_layer.single_layer()
+        extra_layer.extra_layer()
     if rn == '2':
         rn_2 = input('Επιλέξτε υποερώτημα: \n6  (Single-layer MLP)\n7   (Two-Layer MLP)\n8  (Early-Stopping)\n')
         if rn_2 == '6':
